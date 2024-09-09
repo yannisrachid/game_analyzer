7948f2f594dca9ec49e87585ed749a1bbf0c4666
Yannis Rachid <60351404+yannisrachid@users.noreply.github.com>
2024-09-09 17:03:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,10 +42,7 @@ def load_dataframe(league):
 @st.cache_data
 def load_dataframe_s3(league):
     conn = st.connection('s3', type=FilesConnection)
-    league_to_dl = league.replace(" ", "_")
-    st.text(league_to_dl)
-    st.text("footballanalytics/csv_data/{league}_2025_events.csv".format(league=league_to_dl))
-    df = conn.read("footballanalytics/csv_data/{league}_2025_events.csv".format(league=league_to_dl), input_format="csv", ttl=600)
+    df = conn.read("footballanalytics/csv_data/{league}_2025_events.csv".format(league=league), input_format="csv", ttl=600)
     return df
 
 ## Load data in df and select the events from the game chose by the user
